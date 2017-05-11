2c1be5f4a59427f50cda018568d8b11fae918d8d
Ashish Verma <ashish1500616@gmail.com>
2017-05-11 18:35:41 +0530

    added script

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("Script called");
+var sym = ""
+var op = ""
+
+function getD() {
+
+}
+
+function getSymbol(data) {
+    var sym = data;
+    console.log("you selected", sym);
+}
+
+function getOpponent(data) {
+    var op = data;
+    console.log("you selected", op);
+}
