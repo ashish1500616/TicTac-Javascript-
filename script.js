1c7484f4cc47e4fc770c2f4dd28eaa905e76d15c
Ashish Verma <ashish1500616@gmail.com>
2017-05-27 14:00:51 +0530

    workin check functions and seperated funct & dom script

--- script.js
+++ script.js
@@ -1,196 +1,161 @@
 var arr = [
-    [null],
-    [null],
-    [null]
+     [],
+     [],
+     [],
 ];
 console.log("Script called");
-
 var sym = "";
 var op = "";
 var c = 0,
-    r = 0,
-    tr = 0,
-    tl = 0,
-    temp = 2,
-    count = 0;
+   r = 0,
+   tr = 0,
+   tl = 0,
+   temp = 2,
+   count = 0;
 
-function display(){
-for(var i=0;i<3;i++)
+function display()
 {
-    for(var j=0;j<3;j++)
-    {
-        console.log("value at ",i,j,':',arr[i][j]);
-    }
-}
-}
-function getD() {
-
-}
-
-function getSymbol(data) {
-    var sym = data;
-    console.log("you selected", sym);
+   for(var i = 0; i < 3; i++)
+   {
+      for(var j = 0; j < 3; j++)
+      {
+         console.log("value at ", i, j, ':', arr[i][j]);
+      }
+   }
 }
 
-function getOpponent(data){
-    var op = data;
-    if (op === "person") {
-        //console.log("Calling showDiv");
-        showDiv();
-    } else {
-        showDivCom();
-    }
-    console.log("you selected", op);
+
+
+function scores(data)
+{}
+
+/*function check(data)
+{
+   console.log('Inside check Function');
+   console.log("val of data:", data);
+   tr = 0, tl = 0, c = 0;
+   for(var i = 0; i < 3; i++)
+   {
+      r = 0, c = 0;
+      for(var j = 0; j < 3; j++)
+      {
+         // to check row. . .
+         if(arr[i][j] === data)
+         {
+            r++;
+         }
+         else if(arr[j][i] === data)
+         {
+            console.log("index : :", i, j);
+            c++;
+         }
+         else if(i === j && arr[i][j] === data)
+         {
+            tr++;
+         }
+         else if(arr[i][temp] === data)
+         {
+            tl++;
+            --temp;
+         }
+      }
+      console.log("val of c:", c);
+      console.log("val of R:", r);
+      console.log("val of tr:", tr);
+      console.log("val of tl:", tl);
+      if(c == 3 || r === 3 || tr === 3 || tl === 3)
+      {
+         return 1;
+      }
+   }
+}*/
+
+function checkRows()
+{
+   for(var i = 0; i < 3; i++)
+   {
+      if(arr[i][0] == arr[i][1] &&
+         arr[i][1] == arr[i][2] &&
+         arr[i][0] != null)
+         return(true);
+   }
+   return(false);
 }
 
-function scores(data) {
-
-
+function checkCol()
+{
+   for(var i = 0; i < 3; i++)
+   {
+      if(arr[0][i] == arr[1][i] &&
+         arr[1][i] == arr[2][i] &&
+         arr[0][i] != ' ')
+         return(true);
+   }
+   return(false);
 }
 
-function check(data) {
-    console.log('Inside check Function');
+function diagonal()
+{
+   if(arr[0][0] == arr[1][1] &&
+      arr[1][1] == arr[2][2] &&
+      arr[0][0] != ' ')
+      return(true);
 
-    for (var i = 0; i < 3; i++) {
-        for (var j = 0; j < 3; j++) {
-            // to check row
-            if (arr[i][j] === data) {
-                r++;
-            } else if (arr[j][i] === data) {
-                c++;
-            } else if (i === j && arr[i][j] === data) {
-                tr++;
-            } else if (arr[i][temp] === data) {
-                tl++;
-                --temp;
-            }
-        }
-        if (c == 3 || r === 3 || tr === 3 || tl === 3) {
-            return 1;
-        }
+   if(arr[0][2] == arr[1][1] &&
+      arr[1][1] == arr[2][0] &&
+      arr[0][2] != ' ')
+      return(true);
 
-    }
+   return(false);
 }
 
-function showDiv() {
-    //console.log("showDiv called/");
-    document.getElementById("nameDiv").style.display = "block";
-    document.getElementById("nameDivCom").style.display = "none";
 
+
+function counter()
+{
+   count++;
 }
 
-function showDivCom() {
-    //console.log("showDiv called/");
-    document.getElementById("nameDivCom").style.display = "block";
-    document.getElementById("nameDiv").style.display = "none";
-    $("#p2").html("Computer");
 
-}
+function pass()
+{
+   if(count >= 5)
+   {
+      console.log("cr:", checkRows());
+      console.log("cc:", checkCol());
+      console.log("cr:", diagonal());
 
-$("#playerOne").keydown(function(event) {
-    var val = $("#playerOne").val();
-    $("#p1").html(val);
-});
-
-$("#playerOneCom").keydown(function(event) {
-    var val = $("#playerOneCom").val();
-    $("#p1").html(val);
-});
-
-$("#playerTwo").keydown(function(event) {
-    var val = $("#playerTwo").val();
-    $("#p2").html(val);
-});
-
-function counter() {
-    count++;
-}
-
-$('#b1,#b2,#b3,#b4,#b5,#b6,#b7,#b8,#b9').one('click', function(event) {
-    counter();
-    if ($(event.target).attr('id') == 'b1') {
-        if (count % 2 === 0) {
-            $('#b1').html('O');
-            arr[0][0] = 'O';
-        } else {
-            $('#b1').html('X');
-            arr[0][0] = 'X';
-        }
-    } else if ($(event.target).attr('id') == 'b2') {
-        if (count % 2 !== 0) {
-            $('#b2').html('X');
-            arr[0][1] = 'X';
-        } else {
-            $("#b2").html('O');
-            arr[0][1] = 'O';
-        }
-    } else if ($(event.target).attr('id') == 'b3') {
-        if (count % 2 !== 0) {
-            $('#b3').html('X');
-            arr[0][2] = 'X';
-        } else {
-            $('#b3').html('O');
-            arr[0][2] = 'O';
-        }
-    } else if ($(event.target).attr('id') == 'b4') {
-        if (count % 2 !== 0) {
-            $('#b4').html('X');
-            arr[1][0] = 'X';
-        } else {
-            $('#b4').html('O');
-            arr[1][0] = 'O';
-        }
-    } else if ($(event.target).attr('id') == 'b5') {
-        if (count % 2 !== 0) {
-            $('#b5').html('X');
-            arr[1][1] = 'X';
-        } else {
-            $('#b5').html('O');
-            arr[1][1] = 'O';
-        }
-    } else if ($(event.target).attr('id') == 'b6') {
-        if (count % 2 !== 0) {
-            $('#b6').html('X');
-            arr[1][2] = 'X';
-        } else {
-            $('#b6').html('O');
-            arr[1][2] = 'O';
-        }
-    } else if ($(event.target).attr('id') == 'b7') {
-        if (count % 2 !== 0) {
-            $('#b7').html('X');
-            arr[2][0] = 'X';
-        } else {
-            $('#b7').html('O');
-            arr[2][0] = 'O';
-            console.log(arr[2][0]);
-        }
-    } else if ($(event.target).attr('id') == 'b8') {
-        if (count % 2 !== 0) {
-            $('#b8').html('X');
-            arr[2][1] = 'X';
-        } else {
-            $('#b8').html('O');
-            arr[2][1] = 'O';
-        }
-    } else if ($(event.target).attr('id') == 'b9') {
-        if (count % 2 !== 0) {
-            $('#b9').html('X');
-            arr[2][2] = 'X';
-        } else {
-            $('#b9').html('O');
-            arr[2][2] = 'O';
-        }
-    }
-    if (count >= 6) {
-        if (check('O')) {
+      {
+         /*if(data === 'O' && checkRows(data)  || checkCol(data) || diagonal(data))
+         {
             alert("O Full");
-            return;
-        }
-        if (check('X')) {
+         }
+         else if(data === 'X' && checkRows(data) == 1 || checkCol(data) || diagonal(data))
+         {
             alert('X full');
-            return;
-        }
-    }
-    display();
-});
+         }*/
+         if(checkRows() || checkCol() || diagonal())
+         {
+            alert("Full");
+         }
+      }
+   }
+}
+
+function initNewGame()
+{
+   // setup game vars and cards
+   location.reload();
+}
+
+function endGame()
+{
+   // clean game vars and remove cards
+   // alert('Starting new game');
+}
+
+function newOrReset()
+{
+   endGame();
+   initNewGame();
+}
